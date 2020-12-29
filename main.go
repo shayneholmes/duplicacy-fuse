56fdf6ad001eb3125eaeaa83b14bf9dab6bc5b11
Shayne Holmes <github.snth@xoxy.net>
2020-12-29 14:18:32 -0800

    Don't pass duplicacy-specifiec arguments to fuse
    MacFuse errors with errors like "unknown option: repository=foo".

--- main.go
+++ main.go
@@ -15,5 +15,18 @@ func main() {
 
 	duplicacyfs := dpfs.NewDuplicacyfs()
 	host := fuse.NewFileSystemHost(duplicacyfs)
-	host.Mount("", os.Args[1:])
+	// Get fuse-compatible arguments
+	var repository, snapshotid, storageName, storagePassword, loglevel, cachedir string
+	var revision int
+	var debug, all, cleancache bool
+	outargs, err := fuse.OptParse(os.Args, "repository=%s storage=%s snapshot=%s revision=%d password=%s loglevel=%s cachedir=%s debug all cleancache", &repository, &storageName, &snapshotid, &revision, &storagePassword, &loglevel, &cachedir, &debug, &all, &cleancache)
+	if err != nil {
+		log.WithError(err).Fatal("arg error")
+	}
+	if len(outargs) != 2 {
+		log.Fatalf("Expected just one arg, but got: %v", outargs[:1])
+	}
+	log.Errorf("Calling with args: %v", outargs[1:])
+
+	host.Mount("", outargs[1:])
 }
